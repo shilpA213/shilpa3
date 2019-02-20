c458eb4099184bd8ccbd0a5ebeb2f37422270877
shilpA213 <43333451+shilpA213@users.noreply.github.com>
2019-02-20 14:48:37 +0530

    Update and rename README.md to prg4.c

---
+++ prg4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+intmain()
+{int num;
+printf("enter  a number");
+scanf("%d",&num);
+if (num%2=0)
+printf("%d is an even number",num);
+else
+printf("%d is a odd number",num);
+return 0;
+}
